9ca5e1dd8a64fc342887e0ff7faaaadbbe93c3e9
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-03-26 19:24:35 +0000

    routing works

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Layout from './components/Layout';
 import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
+import CategoryPosts from './pages/CategoryPosts';
+import AuthorPosts from './pages/AuthorPosts';
+import Logout from './pages/Logout';
+import UserProfile from './pages/UserProfile';
+import Authors from './pages/Authors';
+import Dashboard from './pages/Dashboard';
 
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
@@ -12,9 +24,20 @@ const router = createBrowserRouter([
     path: '/',
     element: <Layout/>,
     errorElement: <ErrorPage/>,
-    /*children: [
-
-    ]*/
+    children: [
+      {index: true, element: <Home/>},
+      {path: 'posts/:id', element: <PostDetail/>},
+      {path: 'register', element: <Register/>},
+      {path: 'login', element: <Login/>},
+      {path: 'profile/:id', element: <UserProfile/>},
+      {path: 'authors', element: <Authors/>},
+      {path: 'create', element: <CreatePost/>},
+      {path: 'posts/:id/edit', element: <EditPost/>},
+      {path: "posts/categories/:category", element: <CategoryPosts/>},
+      {path: "posts/users/:id", element: <AuthorPosts/>},
+      {path: "myposts/:id", element: <Dashboard/>},
+      {path: "logout", element: <Logout/>}
+    ]
   }
 ])
 
