51eb4354601206ac60b545599c3816c9d2a8ad21
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-03-26 19:53:02 +0000

    dummy header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+import Logo from '../images/logo.png'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+        <div className="container nav__container">
+            <Link to="/" className='nav__logo'>
+              <img src={Logo} alt="Navbar Logo" />
+            </Link>
+            <ul className="nav__menu">
+              <li><Link to="/profile">Cool guy</Link></li>
+              <li><Link to="/create">Create Post</Link></li>
+              <li><Link to="/authors">Authors</Link></li>
+              <li><Link to="/logout">Logout</Link></li>
+            </ul>
+        </div>
+    </nav>
   )
 }
 
