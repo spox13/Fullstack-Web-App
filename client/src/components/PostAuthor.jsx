695b86f61d87313c7d662ddc59a4d84b0187f990
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-03-28 19:13:23 +0000

    author post view

--- client/src/components/PostAuthor.jsx
+++ client/src/components/PostAuthor.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '../images/avatar1.jpg'
 
 const PostAuthor = () => {
-  return (
-    <div>PostAuthor</div>
-  )
+    return (
+        <Link to={`/posts/users/dummy_author`} className="post__author">
+            <div className="post__author-avatar">
+                <img src={Avatar} alt="" />
+            </div>
+            <div className="post__author-details">
+                <h5>By: Michal Jordan</h5>
+                <small>Now</small>
+            </div>
+        </Link>
+    )
 }
 
 export default PostAuthor
