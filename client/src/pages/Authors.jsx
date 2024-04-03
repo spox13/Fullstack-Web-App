cb2e2c3676fcd4e86d94ea8f60339d67b965220a
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-03 18:41:46 +0000

    authors and number of their posts

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import Avatar1 from '../images/avatar1.jpg'
+import Avatar2 from '../images/avatar2.jpg'
+import Avatar3 from '../images/avatar3.jpg'
+import Avatar4 from '../images/avatar4.jpg'
+
+export const DUMMY_AUTHORS = [
+  {
+      id: '1',
+      name: 'John Doe',
+      avatar: Avatar1,
+      posts: 1
+  },
+  {
+      id: '2',
+      name: 'Jane Doe',
+      avatar: Avatar2,
+      posts: 5
+  },
+  {
+      id: '3',
+      name: 'Some Guy',
+      avatar: Avatar3,
+      posts: 3
+  },
+  {
+      id: '4',
+      name: 'Cool Guy',
+      avatar: Avatar4,
+      posts: 8
+  }
+]
 
 const Authors = () => {
+
+  const [authors, setAuthors] = useState(DUMMY_AUTHORS)
+
   return (
-    <div>Authors</div>
+    <section className="authors">
+        {authors.length ? <div className="container authors__container">
+            {authors.map(({id, name, avatar, posts}) => {
+                return <Link key={id} to={`/posts/users/${id}`} className="author">
+                <div className="author__avatar">
+                    <img src={avatar} alt={`Image of ${name}`} />
+                </div>
+                <div className='author__info'>
+                    <h4>{name}</h4>
+                    <p>{posts} posts</p>
+                </div>
+            </Link>
+            })}
+        </div> : <h2 className='center'>No authors found</h2>}
+    </section>
   )
 }
 
