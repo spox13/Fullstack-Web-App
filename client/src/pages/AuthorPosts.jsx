5ab19d38f17d096dce26b093019c77296ba9261f
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-03 17:53:01 +0000

    author of posts view

--- client/src/pages/AuthorPosts.jsx
+++ client/src/pages/AuthorPosts.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DUMMY_POSTS } from '../components/Posts'
+import PostItem from '../components/PostItem'
 
 const AuthorPosts = () => {
+  const [posts, setPosts] = useState(DUMMY_POSTS)
   return (
-    <div>AuthorPosts</div>
+    <section className="author-posts">
+            {posts.length ? <div className="container author-posts__container">
+                {
+                  posts.map(({id, thumbnail, category, title, desc, creator, createdAt}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={creator} createdAt={createdAt}/>)
+                }
+            </div> : <h2 className='center'>No Posts Found.</h2>}
+        </section>
   )
 }
 
