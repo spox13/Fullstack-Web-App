9674df8f8499b748053ffe9a98d96e02d87c4b28
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-04 11:57:36 +0000

    profile page

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import {FiEdit} from 'react-icons/fi'
+import {BiCheck} from 'react-icons/bi'
+
+import Avatar from '../images/avatar10.jpg'
 
 const UserProfile = () => {
+
+  const [avatar, setAvatar] = useState(Avatar)
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmNewPassword, setConfirmNewPassword] = useState('')
+
   return (
-    <div>UserProfile</div>
+    <section className="profile">
+        <div className="container profile__container">
+            <Link to={`/profile/qwe`} className='btn'>My Posts</Link>
+
+            <div className="profile__details">
+                <div className="avatar__wrapper">
+                    <div className="profile__avatar">
+                        <img src={avatar} alt="" />
+                    </div>
+
+                    <form className='avatar__form'>
+                        <input type="file" id='avatar' name='avatar' onChange={e => setAvatar(e.target.files[0])} accept="png, jpg, jpeg" />
+                        <label htmlFor="avatar"><FiEdit/></label>
+                    </form>
+                    <button type="submit" className='profile__avatar-btn'><BiCheck/></button>
+                </div>
+
+                <h1>Dummy Name</h1>
+
+
+                <form className='form profile__form'>
+                    <p className='form__error-message'>This is an error message!</p>
+                    <input type="text" placeholder='Full Name' value={name} onChange={e => setName(e.target.value)} />
+                    <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type="password" placeholder='Current Password' value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} />
+                    <input type="password" placeholder='New Password' value={newPassword} onChange={e => setNewPassword(e.target.value)} />
+                    <input type="password" placeholder='Confirm New Password' value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)} />
+                    <button type="submit" className='btn primary'>Update my details</button>
+                </form>
+            </div>
+            
+        </div>
+    </section>
   )
 }
 
