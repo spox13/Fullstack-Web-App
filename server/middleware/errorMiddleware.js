7a4ffe0f4b7d17c3382b320e0d8bea617a0de000
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-05 11:40:04 +0000

    error handling

---
+++ server/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    req.status(404);
+    next(error)
+}
+
+//handle errors
+const errorHandler = (error, req, res, next) => {
+    if(res.headerSent) {
+        return next(error)
+    }
+
+    res.status(error.code || 500).json({message: error.message || "An unknown error occured"})
+}
+
+
+module.exports = {notFound, errorHandler}
