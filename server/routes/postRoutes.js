05f652404083d00287f4357433210a6526b370ca
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-08 14:21:07 +0000

    creating post model + authorization

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -5,12 +5,12 @@ const authMiddleware = require('../middleware/authMiddleware')
 
 const router = Router()
 
-router.post('/', createPost)
+router.post('/', authMiddleware, createPost)
 router.get('/', getPosts)
 router.get('/:id', getPost)
-router.patch('/:id', editPost)
+router.patch('/:id', authMiddleware, editPost)
 router.get('/categories/:category', getCatPosts)
 router.get('/users/:id', getUserPosts)
-router.delete('/:id', deletePost)
+router.delete('/:id', authMiddleware, deletePost)
 
 module.exports = router
