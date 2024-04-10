865935c06ddacc26bb2e0ebe99d79ac96c4dca3e
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-10 17:24:59 +0000

    combining server with client // now register is fullstack

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
-
   const [userData, setUserData] = useState({
     name: '',
     email: '',
     password: '',
     password2: ''
   })
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
 
   const changeInputHandler = (e) => {
     setUserData(prevState => {
@@ -16,12 +18,27 @@ const Register = () => {
     })
   }
 
+  const registerUser = async (e) => {
+    e.preventDefault();
+    setError('')
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/users/register`, userData)
+        const newUser = await response.data;
+        if(!newUser) {
+            setError("Couldn't register user. Please try again.")
+        }
+        navigate('/login')
+    } catch (err) {
+        setError(err.response.data.message);
+    }
+}
+
   return (
     <section className="register">
       <div className="container">
         <h2>Sign Up</h2>
-        <form className="form register__form">
-          <p className='form__error-message'>This is an error message!</p>
+        <form className="form register__form" onSubmit={registerUser}>
+          {error && <p className='form__error-message'>{error}</p>}
           <input type="text" placeholder='Full Name' name='name' value={userData.name} onChange={changeInputHandler}/>
           <input type="text" placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler}/>
           <input type="password" placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler}/>
