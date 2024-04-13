681b814b15991b575fa39fff6ca24b93bd02439d
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-13 11:29:32 +0000

    fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-app.use(cors({credentials: true, origin: "http://localhost:3000"}))
+app.use(cors({credentials: true, origin: "http://127.0.0.1:3000"}))
 
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
@@ -22,4 +22,4 @@ app.use('/api/posts', postRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-connect(process.env.MONGO_URI).then(app.listen(5000, () => console.log(`Server started on port ${process.env.PORT}`))).catch(error => {console.log(error)})
+connect(process.env.MONGO_URI).then(app.listen(process.env.PORT || 5000, () => console.log(`Server started on port ${process.env.PORT}`))).catch(error => {console.log(error)})
