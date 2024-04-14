9eeb32fda9ef8aec03c945968209d196423012b8
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-14 17:20:21 +0000

    header works correctly

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo.png'
-import {FaBars} from "react-icons/fa"
-import {AiOutlineClose} from "react-icons/ai"
+import {FaBars} from 'react-icons/fa'
+import {AiOutlineClose} from 'react-icons/ai'
+
+import {UserContext} from '../context/userContext'
 
 const Header = () => {
-
-  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
+    const {currentUser} = useContext(UserContext)
+    const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
   
   
     const closeNavHandler = () => {
@@ -17,16 +18,21 @@ const Header = () => {
           setIsNavShowing(true)
         }
       }
-
-  return (
-    <nav>
+  
+    return (
+      <nav>
           <div className="container nav__container">
               <Link to="/" className='nav__logo' onClick={closeNavHandler}>
                 <img src={Logo} alt="Navbar Logo" />
               </Link>
-            
-              { isNavShowing && <ul className='nav__menu'>
-                  <li><Link to={`/profile/qweqwe`} onClick={closeNavHandler}>Dummy Name</Link></li>
+              
+              {!currentUser?.id && isNavShowing && <ul className='nav__menu'>
+                  <li><Link to={'/authors'} onClick={closeNavHandler}>Authors</Link></li>
+                  <li><Link to={'/login'} onClick={closeNavHandler}>Login</Link></li>
+              </ul>}
+              
+              {currentUser?.id && isNavShowing && <ul className='nav__menu'>
+                  <li><Link to={`/profile/${currentUser?.id}`} onClick={closeNavHandler}>{currentUser?.name}</Link></li>
                   <li><Link to={'/create'} onClick={closeNavHandler}>Create Post</Link></li>
                   <li><Link to={'/authors'} onClick={closeNavHandler}>Authors</Link></li>
                   <li><Link to={'/logout'} onClick={closeNavHandler}>Logout</Link></li>
@@ -35,7 +41,7 @@ const Header = () => {
               <button className="nav__toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>{isNavShowing ? <AiOutlineClose/> : <FaBars/>}</button>
           </div>
       </nav>
-  )
-}
+    )
+  }
 
 export default Header
