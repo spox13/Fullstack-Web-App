293f3c422b61ea04d6f8093b0fb96cf876f1ea11
Adam Prus <127137450+spox13@users.noreply.github.com>
2024-04-14 14:10:54 +0000

    login fully works

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,25 +1,45 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+import { UserContext } from '../context/userContext'
 
 const Login = () => {
+  const [userData, setUserData] = useState({email: "", password: ""})
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
 
-  const [userData, setUserData] = useState({
-    email: '', 
-    password: ''
-  })
+  const {setCurrentUser} = useContext(UserContext)
 
   const changeInputHandler = (e) => {
-    setUserData(prevState => {
-        return {...prevState, [e.target.name]: e.target.value}
-    })
-}
+      setUserData(prevState => {
+          return {...prevState, [e.target.name]: e.target.value}
+      })
+  }
+
+  const loginUser = async (e) => {
+      e.preventDefault();
+      setError('')
+      try {
+          const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/users/login`, userData);
+          const user = await response.data;
+          if(!user) {
+              setError("Please check your credentials.")
+          }
+            
+          setCurrentUser(user)
+          navigate("/")
+      } catch (err) {
+          setError(err?.response?.data.message);
+      }
+  }
 
   return (
     <section className="login">
       <div className="container">
         <h2>Sign In</h2>
-        <form className="form login__form">
-          <p className="form__error-message">This is an error message!</p>
+        <form className="form login__form" onSubmit={loginUser}>
+          {error && <p className="form__error-message">{error}</p>}
           <input type="text" placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler} autoFocus/>
           <input type="password" placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler}/>
           <button type="submit" className='btn primary'>Login</button>
