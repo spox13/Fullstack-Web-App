4d91e29364e1a8934d716dfba6defc9b2b828ea0
spox <82903@student.pb.edu.pl>
2024-04-25 21:38:26 +0200

    fetching posts by the category

--- client/src/pages/CategoryPosts.jsx
+++ client/src/pages/CategoryPosts.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react'
-import { DUMMY_POSTS } from '../components/Posts'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 import PostItem from '../components/PostItem'
 
 const CategoryPosts = () => {
-  const [posts, setPosts] = useState(DUMMY_POSTS)
-  return (
-    <section className="posts">
-            {posts.length ? <div className="container posts__container">
-                {
-                  posts.map(({id, thumbnail, category, title, desc, creator, createdAt}) => <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={creator} createdAt={createdAt}/>)
-                }
-            </div> : <h2 className='center'>No Posts Found.</h2>}
-        </section>
-  )
+    const {category} = useParams()
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/categories/${category}`)
+                setPosts(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchPosts();
+    }, [category])
+
+
+    return (
+    <section className="category-posts">
+        {posts.length ? <div className="container posts__container">
+            {
+                posts.map(({_id:id, thumbnail, category, title, description, creator, createdAt}) => {
+                    return <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={description} authorID={creator} createdAt={createdAt}/>
+                })
+            }
+        </div>  : <h2 className="center">No Posts found for {category}.</h2>}
+    </section>
+    )
 }
+
 
 export default CategoryPosts
