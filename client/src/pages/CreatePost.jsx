3ddf9829cf69fa3748128a46d4ba49ed430a929f
spox <82903@student.pb.edu.pl>
2024-04-27 20:41:56 +0200

    create post page fully works

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,6 +13,7 @@ const CreatePost = () => {
   const [category, setCategory] = useState('Uncategorized')
   const [description, setDescription] = useState('')
   const [thumbnail, setThumbnail] = useState('')
+  const [error, setError] = useState('')
   const navigate = useNavigate()
 
   const {currentUser} = useContext(UserContext)
@@ -44,24 +45,50 @@ const formats = [
 
 const POST_CATEGORIES = ["Agriculture", "Business", "Education", "Entertainment", "Art", "Investment", "Uncategorized", "Weather"]
 
+
+const createPost = async (e) => {
+  e.preventDefault();
+
+  const postData = new FormData();
+  postData.set('title', title);
+  postData.set('category', category);
+  postData.set('description', description);
+  postData.set('thumbnail', thumbnail)
+
+  try {
+      const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/posts`, postData, {
+          withCredentials: true, headers: {Authorization: `Bearer ${token}`}
+      })
+      if(response.status == 201) {
+          return navigate('/')
+      }
+  } catch (err) {
+      if(err.response.data.message) {
+          setError("Please choose a thumbnail")
+      } else {
+          setError(err.response.data.message);
+      }
+  }
+}
+
   return (
     <section className="create-post">
-            <div className="container">
-                <h2>Create Post</h2>
-                <p className="form__error-message">This is an error message!</p>
-                <form className='form create-post__form'>
-                    <input type="text" placeholder='Title' value={title} onChange={e => setTitle(e.target.value)} autoFocus />
-                    <select name='category' value={category} onChange={e => setCategory(e.target.value)}>
-                        {
-                            POST_CATEGORIES.map(cat => <option key={cat}>{cat}</option>)
-                        }
-                    </select>
-                    <ReactQuill modules={modules} formats={formats} value={description} onChange={setDescription}></ReactQuill>
-                    <input type="file" onChange={e => setThumbnail(e.target.files[0])} accept="png, jpg, jpeg" />
-                    <button type="submit" className='btn primary'>Create</button>
-                </form>
-            </div>
-        </section>
+      <div className="container">
+          <h2>Create Post</h2>
+            {error && <p className="form__error-message">{error}</p>}
+              <form onSubmit={createPost} className='form create-post__form' encType="multipart/form-data">
+                  <input type="text" placeholder='Title' value={title} onChange={e => setTitle(e.target.value)} autoFocus />
+                  <select name='category' value={category} onChange={e => setCategory(e.target.value)}>
+                      {
+                          POST_CATEGORIES.map(cat => <option key={cat}>{cat}</option>)
+                      }
+                  </select>
+                  <ReactQuill modules={modules} formats={formats} value={description} onChange={setDescription}></ReactQuill>
+                  <input type="file" onChange={e => setThumbnail(e.target.files[0])} accept="png, jpg, jpeg" />
+                  <button type="submit" className='btn primary'>Create</button>
+              </form>
+        </div>
+    </section>
   )
 }
 
